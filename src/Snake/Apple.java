85e0c5dc048d122f505c7f20c965b7117d71ac33
iiChoco <chocowhitemc@gmail.com>
2024-02-08 10:41:00 -0800

    0.0.1

--- src/Snake/Apple.java
+++ src/Snake/Apple.java
@@ -1,23 +1,5 @@
 package Snake;
 
-import utilities.Misc;
-
-import java.awt.*;
-
-public class Apple extends Rectangle {
-
-    public Apple(int x, int y, int width, int height) {
-        super(x, y, width, height);
-    }
-
-    public void relocate() {
-        this.x = (int) (Math.random() * 20) * 50;
-        this.y = (int) (Math.random() * 20) * 50;
-    }
-
-    public void draw(Graphics2D win) {
-        win.drawImage(Misc.loadImage("apple.png"), this.x, this.y, 50, 50, null);
-    }
-
+public class Apple {
 
 }
