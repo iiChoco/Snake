60ee24dd32f8a0efa0644943c1ec7447b7688a71
iiChoco <chocowhitemc@gmail.com>
2024-02-08 12:06:47 -0800

    0.0.4

--- src/Snake/Snake.java
+++ src/Snake/Snake.java
@@ -4,15 +4,18 @@ import utilities.Misc;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Snake extends Rectangle {
 
+    private Map<String, Image> images;
     private final int dir = 0; // 0 = up, 1 = down, 2 = left, 3 = right
     private int lX, lY;
 
     public Snake(int x, int y, int width, int height) {
         super(x, y, width, height);
     }
+
 
     public void move(ArrayList<Snake> s, int ind, int unit) {
         lX = x;
@@ -39,7 +42,12 @@ public class Snake extends Rectangle {
 
     }
 
-
+    public void preloadImages() {
+        String[] parts = {"tail_right", "tail_left", "tail_down", "tail_up", "body_bottomleft", "body_bottomright", "body_topleft", "body_topright", "body_horizontal", "body_vertical"};
+        for (String part : parts) {
+            images.put(part, Misc.loadImage(part + ".png"));
+        }
+    }
     public void draw(Graphics2D pb, int ind) {
         if (ind == 0) {
             switch (dir) {
